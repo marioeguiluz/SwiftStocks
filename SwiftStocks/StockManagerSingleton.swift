1351560df6de78c362fe73181de065d23efe0d36
marioeguiluz <mario@myplacesandme.com>
2015-01-19 23:27:15 +0100

    SDK changes for NSURL.URLWithString(urlString)

--- SwiftStocks/StockManagerSingleton.swift
+++ SwiftStocks/StockManagerSingleton.swift
@@ -27,7 +27,7 @@ class StockManagerSingleton {
         
         //1: YAHOO Finance API: Request for a list of symbols example:
         //http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol IN ("AAPL","GOOG","FB")&format=json&env=http://datatables.org/alltables.env
-       
+        
         //2: Build the URL as above with our array of symbols
         var stringQuotes = "(";
         for quoteTuple in stocks {
@@ -35,9 +35,10 @@ class StockManagerSingleton {
         }
         stringQuotes = stringQuotes.substringToIndex(stringQuotes.endIndex.predecessor())
         stringQuotes = stringQuotes + ")"
-
+        
         var urlString:String = ("http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol IN "+stringQuotes+"&format=json&env=http://datatables.org/alltables.env").stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
-        var url : NSURL = NSURL.URLWithString(urlString)
+        var url : NSURL = NSURL(string: urlString)!
+
         var request: NSURLRequest = NSURLRequest(URL:url)
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         let session = NSURLSession(configuration: config)
@@ -60,7 +61,7 @@ class StockManagerSingleton {
                     var quotes:NSArray = ((jsonDict.objectForKey("query") as NSDictionary).objectForKey("results") as NSDictionary).objectForKey("quote") as NSArray
                     dispatch_async(dispatch_get_main_queue(), {
                         NSNotificationCenter.defaultCenter().postNotificationName(kNotificationStocksUpdated, object: nil, userInfo: [kNotificationStocksUpdated:quotes])
-                        })
+                    })
                 }
             }
         })
